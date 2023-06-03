86a7e99a0b0adfae6e25917f3a51c95ffcda26bf
SMLukwiya <sundaymorganl@gmail.com>
2023-06-03 17:46:37 +0300

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { questions } from "@/questions/advanced"
 import Question from "./components/Question"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-14 w-full">
-      <h1 className='text-3xl font-bold'>JS Quest</h1>
+    <main className="flex min-h-screen flex-col items-center px-14 pt-4 w-full">
+      <Image src='/logo.png' alt="logo" width={100} height={100} />
+      <h1 className='text-3xl font-bold my-2'>JS Quest</h1>
       <div className="w-full md:w-3/4">
         {questions.map((question) => <Question key={question.question} {...question} />)}
       </div>
